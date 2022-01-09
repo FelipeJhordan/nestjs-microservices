1e301d4d2932e3c7a53882cc72f946d72ce75104
Felipe Jhordan <felipejordan.alves@gmail.com>
2022-01-08 21:12:11 -0300

    feat: use modules coming from MongooseModule and Config Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,16 @@
+import { MongooseModule } from '@nestjs/mongoose';
 import { Module } from '@nestjs/common';
+
 import { PlayersModule } from './players/players.module';
+import { mongoUrlWithParams } from './utils/getMongoUrl';
+import { ConfigModule } from '@nestjs/config';
+
 @Module({
-  imports: [PlayersModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(mongoUrlWithParams()),
+    PlayersModule,
+  ],
   controllers: [],
   providers: [],
 })
